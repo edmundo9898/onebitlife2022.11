d3cfaf0d00d9eb4d2c5b8b9b86312a360aa0751b
Edmundo Roberto <Edmundomarinho50@hotmail.com>
2022-11-28 21:16:31 -0300

    Inserindo animação  - LifeStatus e estruturando components

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
   return (
@@ -10,6 +11,7 @@ export default function Start() {
           <Image source={require("../../assets/icons/logo3.png")}
           style={styles.logo}
           />
+          <LifeStatus/>
           <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} {" "} o melhor nivel
           </Text>
