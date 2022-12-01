223298dcd94474dc8137fb70b27d830324e8d31a
Edmundo Roberto <Edmundomarinho50@hotmail.com>
2022-11-30 21:10:16 -0300

    Estrutura das rotas e da página HomePage

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import Allpages from "./AllPages";
+import HomePage from "./HomePage";
+  // se for true vai iniciar pela HomePage, se não vai pela start
 
 export default function Routes(){
-    return <Allpages/> 
+    const [showHome, setShowHome] = useState("false")
+    
+  
+  
+    return <>{showHome === "true" ? <HomePage /> : <Allpages/> }</>;  
 }
+
+
+  
